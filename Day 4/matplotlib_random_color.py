4e2ee71d98bb7f7c57ab1405648a4c7290af6675
onalonedds <160009514+onalonedds@users.noreply.github.com>
2024-02-19 15:52:30 +0500

    Delete Day 4/matplotlib_random_color.py

--- Day 4/matplotlib_random_color.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.colors as mcolors
-import turtle_draws_heart
-def random_color_generator():
-    color = random.choice(list(mcolors.CSS4_COLORS.keys()))
-    return color
-
-random_color = random_color_generator()
-print(random_color)
