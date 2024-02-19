1eac617838feabded05638e81a0c03dbabee3a1c
onalonedds <160009514+onalonedds@users.noreply.github.com>
2024-02-19 15:52:42 +0500

    Delete Day 4/randoms.py

--- Day 4/randoms.py
+++
@@ -1,9 +0,0 @@
-import random
-
-names_str = input("Names list: ")
-names = names_str.split(", ")
-random_integer = random.randint(0, len(names) - 1)
-who_pays = names[random_integer]
-print(who_pays)
-
-
