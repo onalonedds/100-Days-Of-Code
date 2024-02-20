cd11d9acd9197ec4f9c88e38d20687d7c2c5d27c
onalonedds <onalone.dds@gmail.com>
2024-02-21 02:25:41 +0500

    21.02.2024

---
+++ 6_Functions_While_Loops/main.py
@@ -0,0 +1,27 @@
+ # Guess the number game
+import random
+
+range_str = input("Define the range within which the random number is selected: ")
+range_num = range_str.split()
+number = random.randint(int(range_num[0]), int(range_num[1]))
+
+def handle_user_input(user_input):
+    if user_input == "":
+        print("Good bye!")
+        exit()
+    elif not isinstance(user_input, int):
+        print("Your guess is -1. Next time enter a valid number.")
+        return -1
+
+guess = handle_user_input(input("Guess the number I selected, or press Enter to quit: "))
+attempts = 0
+
+while guess != number:
+    if guess < number:
+        guess = handle_user_input(input("Too low, try again, or press Enter to quit."))
+        attempts += 1
+    elif guess > number:
+        guess = handle_user_input(input("Too high, try again, or press Enter to quit."))
+        attempts += 1
+
+print(f"Congratulations! You guessed the right number in {attempts} attempts.")
