0d59b44458a227ad80c0ee842173d00805b77a28
onalonedds <onalone.dds@gmail.com>
2024-02-22 12:43:07 +0500

    22.02.2024

--- 6_7_Functions_While_Loops/CaesarCipher.py
+++ 6_7_Functions_While_Loops/CaesarCipher.py
@@ -7,35 +7,44 @@ chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
          ' ', ',', '.', ':', ';']
 
 def caesar_cipher(action, shift, message):
+    if shift > len(chars):
+        shift = shift % len(chars)
+
     match action:
         case "e":
-            encoded_message = []
+            encoded_message = ""
             for letter in message:
                 index = chars.index(letter) + shift
                 if index >= len(chars):
                     index -= len(chars)
-                encoded_message.append(chars[index])
-            return ''.join(encoded_message)
+                encoded_message += chars[index]
+            return encoded_message
         case "d":
-            decoded_message = []
+            decoded_message = ""
             for letter in message:
                 index = chars.index(letter) - shift
-                decoded_message.append(chars[index])
-            return ''.join(decoded_message)
+                decoded_message += chars[index]
+            return decoded_message
 
-ui_action = ""
-ui_shift = ""
-ui_message = ""
+keep_working = "y"
 
-while ui_action != "e" and ui_action != "d":
-    ui_action = input("Type 'e' to encode, 'd' to decode: ")
+while keep_working == "y":
+    ui_action = ""
+    ui_shift = ""
+    ui_message = ""
 
-while not ui_shift.isnumeric():
-    ui_shift = input("Enter a valid number for shift: ")
+    while ui_action != "e" and ui_action != "d":
+        ui_action = input("Type 'e' to encode, 'd' to decode: ")
 
-while len(ui_message) == 0:
-    ui_message = input("Type your message: ")
+    while not ui_shift.isnumeric():
+        ui_shift = input("Enter a valid number for shift: ")
 
-result = caesar_cipher(ui_action, int(ui_shift), ui_message)
-print(f"Result: {result}")
+    while len(ui_message) == 0:
+        ui_message = input("Type your message: ")
 
+    result = caesar_cipher(ui_action, int(ui_shift), ui_message)
+    print(f"Result: {result}")
+
+    keep_working = input("Continue? y/n ")
+
+print("Good bye!")
