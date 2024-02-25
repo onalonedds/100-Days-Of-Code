3af5be32448fcfb99d95b6ff081e6bc142265d21
onalonedds <onalone.dds@gmail.com>
2024-02-25 10:56:48 +0500

    25.02.2024

--- 10_Call_Func_by_Name/Calculator.py
+++ 10_Call_Func_by_Name/Calculator.py
@@ -24,9 +24,21 @@ operations = {
     "/": divide
 }
 
+print("Operations available:")
+for key in operations:
+    print(f"{key} ({operations[key].__name__})")
+
 while keep_working == "":
-    x = tools.nect_int(input("\nEnter first operand: "))
-    operation = input("Choose operation (+ - * /): ")
+    if 'memory' in globals():
+        x = input("\nEnter first operand or type 'm' to use prev result: ")
+        if x == "m":
+            x = memory
+        else:
+            x = tools.nect_int(x)
+    else:
+        x = tools.nect_int(input("\nEnter first operand: "))
+
+    operation = input("Choose an operation (+ - * /): ")
     y = tools.nect_int(input("Enter second operand: "))
 
     if operation == "/" and y == 0:
@@ -34,5 +46,6 @@ while keep_working == "":
     else:
         action = operations[operation]
         result = action(x, y)
-        print(f"{x} {operation} {y} = {result}")
+        memory = result
+        print(f"{x} {operation} {y} = {result}\nMemory: {result}")
         keep_working = input("Press Enter to continue, any char to exit.")
