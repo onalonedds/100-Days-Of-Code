49abb3f58c556bf5fac4008c01378d8bb78cb171
onalonedds <onalone.dds@gmail.com>
2024-02-28 11:49:32 +0500

    28.02.2024

---
+++ 14/BiggerSmaller.py
@@ -0,0 +1,34 @@
+# Bigger / Smaller Game
+import random
+
+ANIMALS = [
+    "Ant", "Mouse", "Frog", "Sparrow", "Squirrel", "Rabbit", "Cat", "Raccoon", "Fox", "Coyote", "Sheep",
+    "Pig", "Gorilla", "Horse", "Giraffe", "Hippopotamus"
+]
+
+def next_animal(exclude):
+    animals_to_add = ANIMALS[:]
+    animals_to_add.remove(exclude)
+    return random.choice(animals_to_add)
+
+score = 0
+next_round = True
+animals_pair = random.sample(ANIMALS, 2)
+
+while next_round:
+    guess = ""
+    size_1 = ANIMALS.index(animals_pair[0])
+    size_2 = ANIMALS.index(animals_pair[1])
+
+    while guess != "0" and guess != "1":
+        guess = input(f"Who is bigger: {animals_pair[0]} (0) or {animals_pair[1]} (1)? ")
+
+    if animals_pair[int(guess)] == ANIMALS[max(size_1, size_2)]:
+        score += 1
+        print(f"Right! Score: {score}")
+        animals_pair = [animals_pair[1], next_animal(animals_pair[1])]
+    else:
+        print(f"Wrong! Game over. Score: {score}")
+        next_round = False
+
+
