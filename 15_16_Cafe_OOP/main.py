e3888449759f619223bfa4479866fba02bf768ac
onalonedds <onalone.dds@gmail.com>
2024-03-03 21:21:31 +0500

    03.03.2024

--- 15_16_Cafe_OOP/main.py
+++ 15_16_Cafe_OOP/main.py
@@ -7,7 +7,7 @@ cm.bonus_gift()
 print(f"\nDrinks ordered: {cm.drinks_ordered}")
 print(f"Money spent: ${cm.money_spent}")
 
-print(cm.bonus_gifts)
+
 if len(cm.bonus_gifts) > 0:
     print("We have some gifts for you!")
 
