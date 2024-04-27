e7a2478219b92e8fb98f657bc1e2c88dedd14bc8
onalonedds <onalone.dds@gmail.com>
2024-04-27 11:24:12 +0500

    27.04.2024

---
+++ DeepDive_MiscPractice/Decorators/05_Dispatcher.py
@@ -0,0 +1,63 @@
+def dispatcher(fn):
+    registry = dict()
+
+    registry[object] = fn
+
+    # register(type_) - decorator factory
+    def register(type_):
+        def inner(fn):
+            registry[type_] = fn
+            return fn  # we do this so we can stack register decorators!
+
+        return inner
+
+    # decorator(arg) - default decorator
+    def decorator(arg):
+        fn = registry.get(type(arg), registry[object])
+        return fn(arg)
+
+    def get_registry():
+        for k, v in registry.items():
+            print(f'{k.__name__}: {v.__name__}')
+
+    def dispatch(type_):
+        return registry.get(type_, registry[object])
+
+    decorator.register = register
+    decorator.get_registry = get_registry
+    decorator.dispatch = dispatch
+
+    return decorator
+
+
+@dispatcher
+def worker(v):
+    print(f'Default({v})\n')
+
+
+@worker.register(int)
+def worker_i(v):
+    print(f'Integer({v})\n')
+
+
+@worker.register(list)
+def worker_l(v):
+    print(f'List({v})\n')
+
+
+@worker.register(str)
+def worker_s(v):
+    print(f'String({v})\n')
+
+
+@worker.register(bool)
+def worker_b(v):
+    print(f'Boolean({v})\n')
+
+
+worker('Hello!')
+worker(10)
+worker(True)
+worker([1, 2, 3])
+
+worker.get_registry()
--- DeepDive_MiscPractice/Decorators/05_Single_Dispatch.py
+++ DeepDive_MiscPractice/Decorators/05_Single_Dispatch.py
@@ -9,23 +9,23 @@ def worker(v):
 
 
 @worker.register(Integral)
-def worker_d(v):
+def worker_igl(v):
     print(f'Integral({v})\n')
 
 
 @worker.register(Sequence)
-def worker_d(v):
+def worker_sqc(v):
     print(f'Sequence({v})\n')
 
 
-@worker.register(str)
-def worker_d(v):
-    print(f'String({v})\n')
+# @worker.register(str)
+# def worker_str(v):
+#     print(f'String({v})\n')
 
 
-@worker.register(bool)
-def worker_d(v):
-    print(f'Boolean({v})\n')
+# @worker.register(bool)
+# def worker_bool(v):
+#     print(f'Boolean({v})\n')
 
 
 worker('Hello!')
@@ -33,5 +33,6 @@ worker(10)
 worker(True)
 worker([1, 2, 3])
 
-print(worker.registry)
+for k, v in worker.registry.items():
+    print(f'{k.__name__}: {v.__name__}')
 
